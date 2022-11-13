a44e9055f337a5e7a627d9f45e5284124c846bba
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-11-13 21:57:06 +0300

    Add DZ_27

---
+++ DZ_27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Программа, которая принимает на вход число и выдаёт сумму цифр в числе");
+Console.Write("Введите число: ");
+int numA = int.Parse(Console.ReadLine()!);;
+
+int SumNumA(int numA)
+{
+
+    int count = Convert.ToString(numA).Length;
+    int numB = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        numB = numA - numA % 10;
+        result = result + (numA - numB);
+        numA = numA / 10;
+    }
+    return result;
+}
+
+int sumNumA = SumNumA(numA);
+Console.WriteLine("Сумма цифр : " + sumNumA);
